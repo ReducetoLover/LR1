6f3ba640ca7618c22a9fe6657f9fe48774038f4b
ReducertoLover <krasnovskii2001@mail.ru>
2022-02-19 18:51:36 +0700

    hgh

--- LR1/Program.cs
+++ LR1/Program.cs
@@ -13,26 +13,38 @@ namespace LR1
     //a  b границы интервала  сгенерировать N больше случ чисел от а до b
     static void LAb1()
     {
-      double all=0;
+      
+      double Tall=0;
+      double all = 0;
       Console.Write("A=");
       double a = Convert.ToDouble(Console.ReadLine());
       Console.Write("B=");
       double b = Convert.ToDouble(Console.ReadLine());
       Console.Write("N=");
       int N = Convert.ToInt32(Console.ReadLine());
+      double[] ARR = new double[N];
       for (int i = 0; i < N; i++)
       {
         Random rnd = new Random();
         double r = rnd.NextDouble();
         double R = a + (b - a) * r;
-        all += R;
+        ARR[i] = R;
+        Tall += R;
         Console.WriteLine(R);
       }
       double Tmath = (a + b) / 2;
       double Tdis = Math.Pow((b - a),2) / 12;
-      double Pmath = all / N;
-      Console.WriteLine("Тмат= " +Tmath);
-      Console.WriteLine("мат= " + Pmath);
+      double Pmath = Tall / N;
+      for (int i = 0; i < ARR[N]; i++)
+      {
+        all += Math.Pow((ARR[i] - Tall), 2);
+      }
+      double Pdis = all / N;
+      Console.WriteLine("Тматожидание= " +Tmath);
+      Console.WriteLine("Оценкаматожидание= " + Pmath);
+      Console.WriteLine("Тдисперсия= " + Tdis);
+      Console.WriteLine("Оценкадисперсии= " + Pdis);
+
     }
   }
 }
