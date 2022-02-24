a5c58373d22e10e1f4bc88136d52ea9bf04323b7
ReducertoLover <krasnovskii2001@mail.ru>
2022-02-24 21:48:23 +0700

    new

--- LR1/Program.cs
+++ LR1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LR1
 {
@@ -13,7 +14,10 @@ namespace LR1
     //a  b границы интервала  сгенерировать N больше случ чисел от а до b
     static void LAb1()
     {
-      
+      double XI = 0;
+      double p = 0;
+      double LensInterval = 0;
+      double k = 0;
       double Tall=0;
       double all = 0;
       Console.Write("A=");
@@ -35,11 +39,28 @@ namespace LR1
       double Tmath = (a + b) / 2;
       double Tdis = Math.Pow((b - a),2) / 12;
       double Pmath = Tall / N;
-      for (int i = 0; i < ARR[N]; i++)
+      for (int i = 0; i < N; i++)
       {
-        all += Math.Pow((ARR[i] - Tall), 2);
+        all += Math.Pow((ARR[i] - Pmath), 2);
       }
-      double Pdis = all / N;
+      double Pdis =Math.Sqrt(all / N);
+
+      k = Math.Round(1 + 3.219 * Math.Log10(N));
+      p = 1 / k;
+      // double[] intervals = new double[Convert.ToInt32(k)];
+      var intervals = new KeyValuePair<double, double>();
+      LensInterval = Math.Round((b - a) / k,3);
+      int f = 10;
+      //for (int i = 0; i < ARR.Length; i++)
+      //{
+      //  intervals[f % (i + 1)]= ARR[i];
+      //}
+
+      //Console.WriteLine(intervals.Count);
+      Console.WriteLine("K= "+k);
+      Console.WriteLine("длина интервала= " + LensInterval);
+
+
       Console.WriteLine("Тматожидание= " +Tmath);
       Console.WriteLine("Оценкаматожидание= " + Pmath);
       Console.WriteLine("Тдисперсия= " + Tdis);
